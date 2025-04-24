a282d52f05326ff36cb96a3cf50db74e7a431f31
Daryna <kapitanckyk.dasha@gmail.com>
2025-04-24 16:27:11 +0300

    Updates

---
+++ index.f75de5e1.js
@@ -0,0 +1,30 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve)=>{
+        function handler() {
+            const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+            resolve(message);
+        // element.removeEventListener(eventName, handler);
+        }
+        element.addEventListener(eventName, handler);
+    });
+}
+const printMessage = (message)=>{
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.textContent = message;
+    document.body.appendChild(div);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
